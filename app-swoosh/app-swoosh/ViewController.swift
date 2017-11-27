d80beecf1c6f257f54d07445d03cdf4d8647791e
Barış Cesim <berva@Bars-MacBook-Pro.local>
2017-11-27 13:41:57 +0300

    Swoosh Checkpoint

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Barış Cesim on 27.11.2017.
+//  Copyright © 2017 bcesim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgimg: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2 , y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        bgimg.frame = view.frame
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
