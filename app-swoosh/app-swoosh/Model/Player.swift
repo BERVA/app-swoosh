0c60b09c8510c72bf657e0a40ade0b454db62b50
Barış Cesim <berva@Bars-MacBook-Pro.local>
2017-11-27 18:37:06 +0300

    Final

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  app-swoosh
+//
+//  Created by Barış Cesim on 27.11.2017.
+//  Copyright © 2017 bcesim. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
